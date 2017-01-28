f3097c243c1780d6e609405da1684837704306fa
Sergey Lapin <lapanoid@gmail.com>
2017-01-29 00:41:05 +0300

    refactor: use async in wrapMaybeChangePort instead of promises

--- src/server/wrapMaybeChangePort.js
+++ src/server/wrapMaybeChangePort.js
@@ -9,37 +9,34 @@ import chalk from 'chalk';
 const DEFAULT_PORT = process.env.PORT || 3000;
 
 export default (run: Function, { isInteractive }: { isInteractive: boolean }) =>
-  () => {
+  async () => {
     // We attempt to use the default port but if it is busy, we offer the user to
     // run on a different port. `detect()` Promise resolves to the next free port.
-    detect(DEFAULT_PORT).then(port => {
-      if (port === DEFAULT_PORT) {
+    const port = await detect(DEFAULT_PORT);
+    if (port === DEFAULT_PORT) {
+      run(port);
+      return;
+    }
+
+    if (isInteractive) {
+      clearConsole();
+      const existingProcess = getProcessForPort(DEFAULT_PORT);
+      const question = chalk.yellow(
+        'Something is already running on port ' +
+          DEFAULT_PORT +
+          '.' +
+          (existingProcess ? ' Probably:\n  ' + existingProcess : '')
+      ) +
+        '\n\nWould you like to run the app on another port instead?';
+
+      const shouldChangePort = await prompt(question, true);
+
+      if (shouldChangePort) {
         run(port);
-        return;
       }
-
-      if (isInteractive) {
-        clearConsole();
-        const existingProcess = getProcessForPort(DEFAULT_PORT);
-        const question = chalk.yellow(
-          'Something is already running on port ' +
-            DEFAULT_PORT +
-            '.' +
-            (existingProcess ? ' Probably:\n  ' + existingProcess : '')
-        ) +
-          '\n\nWould you like to run the app on another port instead?';
-
-        prompt(question, true).then(shouldChangePort => {
-          if (shouldChangePort) {
-            run(port);
-          }
-        });
-      } else {
-        console.log(
-          chalk.red(
-            'Something is already running on port ' + DEFAULT_PORT + '.'
-          )
-        );
-      }
-    });
+    } else {
+      console.log(
+        chalk.red('Something is already running on port ' + DEFAULT_PORT + '.')
+      );
+    }
   };
