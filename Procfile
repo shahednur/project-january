50b055ef4935ad4b3968fd60026d14c5b1d36cae
Sergey Lapin <lapanoid@gmail.com>
2017-01-29 21:44:47 +0300

    wip: try to deploy another process

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: npm run start
 worker: npm run start-api
+web: npm run start
--- src/server/index.babel.js
+++ src/server/index.babel.js
@@ -253,3 +253,34 @@ const run = port => {
 };
 
 wrapMaybeChangePort(run, { isInteractive, clearConsole })();
+
+const WebSocketClient = require('websocket').client;
+
+const client = new WebSocketClient();
+
+client.on('connectFailed', error => {
+  console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', connection => {
+  console.log('WebSocket Client Connected');
+  connection.on('error', error => {
+    console.log('Connection Error: ' + error.toString());
+  });
+  connection.on('close', () => {
+    console.log('echo-protocol Connection Closed');
+  });
+  connection.on('message', message => {
+    if (message.type === 'utf8') {
+      console.log("Received: '" + message.utf8Data + "'");
+    }
+  });
+  setInterval(
+    () => {
+      connection.sendUTF('web - ping');
+    },
+    2000
+  );
+});
+
+client.connect('ws://localhost:4000/', 'echo-protocol');
