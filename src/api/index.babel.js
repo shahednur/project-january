d0599532bf1d9e7a74feaa254510dd19056aeaf7
Sergey Lapin <lapanoid@gmail.com>
2017-01-29 15:19:14 +0300

    wip: add koa simple websocket server

--- src/api/index.babel.js
+++ src/api/index.babel.js
@@ -1,14 +1,45 @@
 //@flow
 
 import Koa from 'koa';
+import route from 'koa-route';
+import Kefir from 'kefir';
+import websockify from 'koa-websocket';
 
-// Koa application is now a class and requires the new operator.
-const app = new Koa();
+const app = websockify(new Koa());
 
-// uses async arrow functions
+app.ws.use(
+  route.all('/', ({ websocket }) => {
+    const send = json => websocket.send(JSON.stringify(json));
+    const usualCIText = [
+      'git clone repo xxx',
+      'yarn/npm install',
+      'build...',
+      'deploy...',
+      'run app'
+    ];
+
+    Kefir.sequentially(1000, usualCIText).observe({
+      value(value) {
+        send({ type: 'CI', value });
+      },
+      error(error) {
+        send(error);
+      },
+      end() {
+        send({ type: 'CI', value: 'end' });
+      }
+    });
+    send({ type: 'SYSTEM', value: 'Hello World' });
+    websocket.on('message', message => {
+      console.log(message);
+      send({ type: 'SYSTEM', value: message + '!' });
+    });
+  })
+);
+
 app.use(async (ctx, next) => {
   try {
-    await next(); // next is now a function
+    await next();
   } catch (err) {
     ctx.body = { message: err.message };
     ctx.status = err.status || 500;
@@ -16,8 +47,6 @@ app.use(async (ctx, next) => {
 });
 
 app.use(async ctx => {
-  // throw Error('error');
-  // await instead of yield
   ctx.body = { result: 'lol' }; // ctx instead of this
 });
 
